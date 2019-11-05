c25b336434dc6644359919c1c19d6e01f14e4984
Brandon Heck <bhecky99@gmail.com>
2019-11-05 09:17:58 -0500

    Began creating php page from the html pages. We moved the header and footer into include files.

---
+++ include/footer.inc.php
@@ -0,0 +1,14 @@
+<footer>
+
+<h4>this is the footer</h4>
+<p>we place redudant information here</p>
+
+<div id="footerTagLine">
+    I am 7 inches left of normal
+</div>
+
+</footer>
+
+</body>
+
+</html>
